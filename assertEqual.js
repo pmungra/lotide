defe5dbfe976980effbbb3b6f644a686fd8ddd69
parth <parthpmungra@gmail.com>
2021-11-23 10:12:39 +0000

    Implement assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,40 @@
+// Without Calling function value
+
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅  Assertion Passed: actual === expected");
+  } else console.log("🛑🛑🛑 Assertion failed: actual !== expected");
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hi", "Hello");
+assertEqual(7, 8);
+assertEqual(7, 7);
+
+break;
+
+// Without ES6 Template Literals
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅  Assertion Passed: " + actual + "===" + expected);
+  } else console.log("🛑🛑🛑 Assertion failed: "+ actual+ "!==" + expected);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("Hi", "Hello");
+assertEqual(7, 8);
+assertEqual(7, 7);
+*/
+
+//---------------------------------------------------------------------------------------------------
+// ES6 Template Literals
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
@@ -10,4 +47,3 @@ assertEqual("Hello", "Hello");
 assertEqual("Hi", "Hello");
 assertEqual(7, 8);
 assertEqual(7, 7);
-
