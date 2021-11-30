3d836003fea86335d01ca1abc7aa140dd6ac53d2
Your Name <parth.mungra94@gmail.com>
2021-11-30 21:45:31 +0000

    headTest.js With Mocha & Chai

---
+++ test/headTest.js
@@ -0,0 +1,32 @@
+
+//TEST CODE
+
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+  it("returns 'Hello' for ['Hello']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+  it("returns '[]' for 5", () => {
+    assert.strictEqual(head([]), 5); 
+  });
+  it("returns '[7]' for 7", () => {
+    assert.strictEqual(head([7]), 7); 
+  });
+  it(`returns '["Hello"]'for ["Hello"]`, () => {
+    assert.strictEqual(head(["Hello"]), ["Hello"]); 
+  });
+  it("returns '[]' for []", () => {
+    assert.strictEqual(head([]), []); 
+  });
+});
