6186ea20befd61bbec9db1e395c19c2ea1aa6e86
Your Name <parth.mungra94@gmail.com>
2021-11-30 19:21:05 +0000

    Refactored head

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-};
-
+const assertEqual = require('./assertEqual');
 
 let head = function(inputArray) {
   return (inputArray[0]);
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([7]), 7);
-assertEqual(head(["Hello"]), ["Hello"]);
-assertEqual(head([]), []);
+module.exports = head; 
