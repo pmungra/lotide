0c7648b9b907042836434fe595b4735ee33e6ecf
Your Name <parth.mungra94@gmail.com>
2021-11-30 20:14:14 +0000

    Updated the parameter name

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,12 @@
 const assertEqual = require('./assertEqual');
 
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
+
+const eqArrays = function(actual,expected) {
+  if (actual.length !== expected.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
       return false;
     }
   }
