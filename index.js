7e81868048efac3a463358c974e291d5136a7f1e
Your Name <parth.mungra94@gmail.com>
2021-12-02 01:39:38 +0000

    Updated the function list

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqObjects = require('./eqObjects');
+const without = require('./without');
+const findKey = require('./findKey');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
-};
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  findKey,
+  map,
+  takeUntil,
+  assertObjectsEqual,
+  eqObjects,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
+  without
+}
